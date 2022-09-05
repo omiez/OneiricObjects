f02ffe84a7d1b31fe8e8743a923d37ac543fb0bc
omiez <amramaz@gmail.com>
2022-09-05 18:27:00 +0300

    fix ommited s3 module

--- cms/config/env/production/database.js
+++ cms/config/env/production/database.js
@@ -15,20 +15,7 @@ module.exports = ({ env }) => ({
           rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
         },
       },
-      options: {
-        debug: false,
-        pool: {
-          min: 0,
-          max: 100,
-          acquireTimeoutMillis: 300000,
-          createTimeoutMillis: 300000,
-          destroyTimeoutMillis: 50000,
-          idleTimeoutMillis: 300000,
-          reapIntervalMillis: 10000,
-          createRetryIntervalMillis: 2000,
-          propagateCreateError: false
-        }
-      },
+      options: {}
     },
   },
 });
